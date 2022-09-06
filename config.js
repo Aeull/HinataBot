1fb084ae0d561e13eaaa0cb53bcdca1e5aa47197
Aeull <110038479+Aeull@users.noreply.github.com>
2022-09-06 16:18:11 +0800

    Update config.js

--- config.js
+++ config.js
@@ -6,16 +6,16 @@ import moment from 'moment-timezone'
 
 /*Oᴡɴᴇʀ number*/
 global.owner = [
-  ['6282195322106', '️One', true],
-  ['6282195322106', '️Two', true],
-  ['6282195322106', '️Three', true],
-  ['6282195322106', '️Four', true],
-  ['6282195322106', '️Five', true],
-  ['6282195322106', '️Six', true],
-  ['6282195322106', '️Seven', true],
-  ['6282195322106', '️Eight', true],
-  ['6282195322106', '️Nine', true],
-  ['6282195322106', '️Teen', true]
+  ['6285822347348', '️One', true],
+  ['6285822347348', '️Two', true],
+  ['6285822347348', '️Three', true],
+  ['6285822347348', '️Four', true],
+  ['6285822347348', '️Five', true],
+  ['6285822347348', '️Six', true],
+  ['6285822347348', '️Seven', true],
+  ['6285822347348', '️Eight', true],
+  ['6285822347348', '️Nine', true],
+  ['6285822347348', '️Teen', true]
 ]
 global.mods = []
 global.prems = []
@@ -82,15 +82,15 @@ global.xckey = 'BOhjM2hUVn'
 */
 
 /*Number*/
-global.nomorbot = '6285171216276'
-global.nomorown = '6282195322106'
-global.namebot = 'ʜɪɴᴀᴛᴀ-ʙᴏᴛ'
+global.nomorbot = '6283843192208'
+global.nomorown = '6285822347348'
+global.namebot = 'ArullBotz'
 global.nameown = '「 Oᴡɴᴇʀ 」'
 
 /*Watermark*/
-global.wm = '                「 ʜɪɴᴀᴛᴀ-ʙᴏᴛ あ⁩ 」'
-global.wm2 = '꒷︶꒷꒥꒷ ‧₊˚ ꒰ฅ˘օառɛʀ˘ฅ ꒱ ‧₊˚꒷︶꒷꒥꒷'
-global.wm3 = '⫹⫺ ʜɪɴᴀᴛᴀ-ʙᴏᴛ'
+global.wm = '「 ArullBotz⁩ 」'
+global.wm2 = '˘օառɛʀ˘'
+global.wm3 = 'ArullBotz'
 global.botdate = `⫹⫺ Date :  ${moment.tz('Asia/Jakarta').format('DD/MM/YY')}`
 global.bottime = `𝗧 𝗜 𝗠 𝗘 : ${moment.tz('Asia/Jakarta').format('HH:mm:ss')}`
 global.titlebot = `⫹⫺ Time Sever : ${moment.tz('Asia/Jakarta').format('HH:mm:ss')}\n⫹⫺ Date Server :  ${moment.tz('Asia/Jakarta').format('DD/MM/YY')}`
@@ -385,13 +385,13 @@ global.hwaifu = ['https://i.pinimg.com/originals/ed/34/f8/ed34f88af161e6278993e1
 'https://i.pinimg.com/originals/80/4f/1a/804f1a05f9996c96a2d492b4854b7fd5.jpg']
 
 /*Link*/
-global.sig = 'https://www.instagram.com/notifikasi_pemberitahuan'
-global.sgh = 'https://www.github.com/AyGemuy'
-global.sgc = 'https://chat.whatsapp.com/BAs2c2UeLE8AaI806UWUvI'
-global.sdc = 'https://www.discord.com/wudysoft'
-global.snh = 'https://www.tiktok.com/@upload_file'
-global.sfb = 'https://www.facebook.com/'
-global.syt = 'https://www.youtube.com/'
+global.sig = 'https://www.instagram.com/sahrulwara_____'
+global.sgh = 'https://www.github.com/Warikrr'
+global.sgc = 'https://chat.whatsapp.com/ItwCvXL02hg11U5qNaLSBe'
+global.sdc = 'https://youtu.be/NmP2bAEOI9g'
+global.snh = 'https://youtu.be/Y4lDHozWsnI'
+global.sfb = 'https://youtube.com/shorts/vIr_jDSN5UY?feature=share'
+global.syt = 'https://youtube.com/shorts/vIr_jDSN5UY?feature=share'
 
 /*Nsfw*/
 global.optsnsfw = true
@@ -432,9 +432,9 @@ global.htjava = '⫹⫺'
 
 /*Watermark*/
 global.stickpack = '.'
-global.stickauth = `☂︎ ʜɪɴᴀᴛᴀ-ʙᴏᴛ\n⫹⫺ 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩\nwa.me/${global.nomorbot}`
-global.packname = '☂︎ ᴄʀᴇᴀᴛᴇᴅ ʙʏ'
-global.author = '𝗛𝗶𝗻𝗮𝘁𝗮 乂 𝘄𝘂𝗱𝘆𝘀𝗼𝗳𝘁'
+global.stickauth = `☂︎ArullBotz\n⫹⫺ 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩\nwa.me/${global.nomorbot}`
+global.packname = '☂︎ Arull'
+global.author = 'ArullBotz'
 global.multiplier = 10
 
 /*Pesan*/
@@ -443,21 +443,21 @@ global.wait = '*ᴡᴀɪᴛ!*'
 global.render = '*ʀᴇɴᴅᴇʀ!*'
 global.webs = 'https://s.id/Cerdasin62'
 global.logo = 'https://i.pinimg.com/564x/f7/d2/e4/f7d2e48fd59a8c01cd396bfc70b0a2d1.jpg'
-global.gcwangsaf = 'https://chat.whatsapp.com/Fxz8FpQ0ddr2GrEjfYkJ1E'
+global.gcwangsaf = 'https://chat.whatsapp.com/ItwCvXL02hg11U5qNaLSBe'
 
 /*Donasi*/
-global.saweria = 'https://saweria.co/wudysoft'
-global.dana = '0887435373103'
-global.pulsa = '082195322106'
-global.trakteer = 'https://trakteer.id/wudysoft'
-global.paypal = 'wudysoft@mail.com'
-global.gopay = '082195322106'
-global.pdana = '0887435373103'
-global.povo = '082195322106'
-global.pgopay = '082195322106'
-global.ppulsa = '082195322106'
-global.ppulsa2 = '082195322106'
-global.psaweria = 'https://saweria.com/Wudysoft'
+global.saweria = 'https://saweria.co/arullofc'
+global.dana = '085822347348'
+global.pulsa = '085822347348'
+global.trakteer = ''
+global.paypal = ''
+global.gopay = ''
+global.pdana = '085822347348'
+global.povo = ''
+global.pgopay = ''
+global.ppulsa = '085822347348'
+global.ppulsa2 = '083843192208'
+global.psaweria = 'https://saweria.com/arullofc'
 
 /*Emoji*/
 global.rpg = {
